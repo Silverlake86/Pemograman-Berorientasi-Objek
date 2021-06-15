023946a0ae45bc08909e187ac4747664794eabd9
Silverlake86 <78220117+Silverlake86@users.noreply.github.com>
2021-06-15 22:18:24 +0700

    Update App.java

--- Pertemuan 5/App.java
+++ Pertemuan 5/App.java
@@ -1,13 +1,13 @@
 import edu.uph.SIAK.model.jadwalkuliah;
 import edu.uph.SIAK.model.mahasiswa;
-import edu.uph.SIAK.model.dosen;
-public class App {
-    public static void main(String[] args) throws Exception {
-        mahasiswa mhs1 = new mahasiswa("Clavin", "01210121",false);
-        mahasiswa mhs2 = new mahasiswa("SriRahayu", "01210121",true);
-        mahasiswa mhs3 = new mahasiswa("Elvia", "01210121",true);
 
+public class app {
+    public static void main(String[] args) throws Exception {
         
+        mahasiswa mhs1 = new mahasiswa("Alvin Yogie", "03082180031",false);
+        mahasiswa mhs2 = new mahasiswa("Michael Salim", "03082180035",true);
+        mahasiswa mhs3 = new mahasiswa("Elvia Clarissa", "03082180037",true);
+    
         jadwalkuliah JadwalKuliah = new jadwalkuliah();
         JadwalKuliah.tambahMahasiswa(mhs1);
         JadwalKuliah.tambahMahasiswa(mhs2);
